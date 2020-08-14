a7a5e07e3646c7b2e3cbc55824173bb6f9446fbf
Frederic Mercille <fmercille@gmail.com>
2020-08-14 15:47:27 -0400

    Exercise 7.5

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -75,10 +75,17 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
           <input {...content} />
@@ -91,7 +98,7 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button><button type="reset">reset</button>
       </form>
     </div>
   )
